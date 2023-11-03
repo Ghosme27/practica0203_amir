4f254ccaa287df69d870c4a08c6c629f02433dd1
Ghosme27 <113557448+Ghosme27@users.noreply.github.com>
2023-11-03 23:26:08 +0100

    Update ejercicio9.py

--- ejercicio9.py
+++ ejercicio9.py
@@ -2,6 +2,6 @@ palabra=input('introduzca una palabra:')
 vocales=['a','e','i','o','u']
 contador=[0]*len(vocales)
 for i in palabra:
-    if i==len(vocales):
-        contador[len(vocales)] +=1
+    if i==vocales:
+        contador[vocales] +=1
 print('la palabra',palabra,'de todas estas vocales',vocales,'aparece este numero de veces',contador)
